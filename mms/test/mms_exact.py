e63cf294340aa81e5e066455dfeac8ae57bb23ce
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-07-02 07:17:51 -0600

    Move python MMS tests into python directory
    (refs #12049)

---
+++ mms/test/mms_exact.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
+import mms
+fs,ss = mms.evaluate('-div(grad(u))', 'sin(2*pi*x)*sin(2*pi*y)')
+mms.print_fparser(fs)
+
+mms.print_hit(fs, 'force')
+mms.print_hit(ss, 'exact')
+
+ft,st = mms.evaluate('diff(u,t) - div(grad(u))', 't**3*x*y')
+mms.print_fparser(ft)
+
+mms.print_hit(ft, 'force')
+mms.print_hit(st, 'exact')
