49f34a5baaf0e45e12ad4c39915eca62ea910ddb
Andrew E Slaughter <slaughter98@gmail.com>
2021-02-18 17:53:18 -0700

    Update python headers
    (refs #23)

--- parameters/Parameter.py
+++ parameters/Parameter.py
@@ -1,11 +1,13 @@
-#* This file is part of the MOOSE framework
-#* https://www.mooseframework.org
+#* This file is part of MOOSETOOLS repository
+#* https://www.github.com/idaholab/moosetools
 #*
 #* All rights reserved, see COPYRIGHT for full restrictions
 #* https://github.com/idaholab/moose/blob/master/COPYRIGHT
 #*
 #* Licensed under LGPL 2.1, please see LICENSE for details
 #* https://www.gnu.org/licenses/lgpl-2.1.html
+
+
 import textwrap
 import inspect
 import logging
--- parameters/test/test_InputParameters.py
+++ parameters/test/test_InputParameters.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 #pylint: disable=missing-docstring
-#* This file is part of the MOOSE framework
-#* https://www.mooseframework.org
+#* This file is part of MOOSETOOLS repository
+#* https://www.github.com/idaholab/moosetools
 #*
 #* All rights reserved, see COPYRIGHT for full restrictions
 #* https://github.com/idaholab/moose/blob/master/COPYRIGHT
 #*
 #* Licensed under LGPL 2.1, please see LICENSE for details
 #* https://www.gnu.org/licenses/lgpl-2.1.html
+
+
 import sys
 import re
 import unittest
