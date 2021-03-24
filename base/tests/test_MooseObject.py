d2947881b0f58850c11625018db9036878fd71fd
Andrew E Slaughter <slaughter98@gmail.com>
2021-03-23 22:08:33 -0600

    Update headers
    (refs #40)

--- base/tests/test_MooseObject.py
+++ base/tests/test_MooseObject.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+#* This file is part of MOOSETOOLS repository
+#* https://www.github.com/idaholab/moosetools
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moosetools/blob/main/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
 import unittest
 import logging
 import parameters
