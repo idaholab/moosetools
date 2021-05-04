b5bfa78ba12e2ebc9262766c90131310c1f63657
Andrew E Slaughter <slaughter98@gmail.com>
2021-05-03 21:51:14 -0600

    Improving docs and starting on Controller testing
    (refs #9999)

---
+++ moosetools/moosetest/tests/test_Controller.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import io
+import logging
+import unittest
+from moosetools.parameters import InputParameters
+from moosetools.base import MooseException
+from moosetools import moosetest
+
+class TestController(unittest.TestCase):
+    def testDefault(self):
+
+        # name is required
+        with self.assertRaises(MooseException) as ex:
+            runner = moosetest.base.Controller()
+        self.assertIn("The parameter 'name' is marked as required", str(ex.exception))
+
+        runner = moosetest.base.Controller(name='name')
+        self.assertIsNotNone(runner.getParam('_unique_id'))
+        self.assertIsNone(runner.getParam('differs'))
+
+        with self.assertRaises(NotImplementedError) as ex:
+            runner.execute()
+        self.assertIn("The 'execute' method must be overridden.", str(ex.exception))
+
+    def testControllers(self):
+        class ProxyController(object):
+
+            @staticmethod
+            def validObjectParams():
+                params = InputParameters()
+                params.add('platform')
+                return params
+
+            def getParam(self, value):
+                return 'test'
+
+        runner = moosetest.base.Controller(None, [ProxyController(),], name='name', test_platform='TempleOS')
+        self.assertIn('test', runner.parameters())
+        self.assertIn('platform', runner.getParam('test'))
+        self.assertEqual(runner.getParam('test_platform'), 'TempleOS')
+
+if __name__ == '__main__':
+    unittest.main(module=__name__, verbosity=2)
