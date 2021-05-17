2e30efc794dccb41ccc8ca193b8137caf3976c74
Andrew E Slaughter <slaughter98@gmail.com>
2021-05-17 17:45:44 -0600

    Tests for ConsoleDiff
    (fixes #9999)

--- moosetools/moosetest/differs/ConsoleDiff.py
+++ moosetools/moosetest/differs/ConsoleDiff.py
@@ -1,26 +1,42 @@
-import enum
-import math
+import re
 from moosetools.moosetest.base import Differ
 
 class ConsoleDiff(Differ):
+    """
+    A tool for testing for the existence of text within `sys.stdour` and/or `sys.stderr`.
+    """
 
     @staticmethod
     def validParams():
         params = Differ.validParams()
-        # TODO: text_in_out
-        # TODO: text_notin_out
-
+        params.add('text_in', vtype=str, doc="Checks that the supplied text exists in sys.stdout or sys.stderr.")
+        params.add('text_not_in', vtype=str, doc="Checks that the supplied text does not exist in sys.stdout and sys.stderr.")
         params.add('text_in_stdout', vtype=str, doc="Checks that the supplied text exists in sys.stdout.")
         params.add('text_not_in_stdout', vtype=str, doc="Checks that the supplied text does not exist in sys.stdout.")
         params.add('text_in_stderr', vtype=str, doc="Checks that the supplied text exists in sys.stderr.")
         params.add('text_not_in_stderr', vtype=str, doc="Checks that the supplied text does not exist in sys.stderr.")
-
+        params.add('re_match', vtype=str, doc="Checks that the supplied regular expression returns a match in sys.stdout or sys.stderr.")
+        params.add('re_match_stdout', vtype=str, doc="Checks that the supplied regular expression returns a match in sys.stdout.")
+        params.add('re_match_stderr', vtype=str, doc="Checks that the supplied regular expression returns a match in sys.stderr.")
+        params.add('re_flags', vtype=str, array=True, default=('MULTILINE', 'DOTALL', 'UNICODE'),
+                   allow=('MULTILINE', 'DOTALL', 'UNICODE', 'IGNORECASE', 'VERBOSE', 'LOCALE', 'DEBUG', 'ASCII'),
+                   doc="The names of the flags to pass to regular expression `match` function.")
         return params
 
-    #def __init__(self, *args, **kwargs):
-    #    Differ.__init__(self, *args, **kwargs)
-
     def execute(self, rcode, stdout, stderr):
+
+        # STDOUT/STDERR
+        text_in = self.getParam('text_in')
+        if (text_in is not None) and (text_in not in stdout) and (text_in not in stderr):
+            msg = "The content of 'text_in' parameter, '{}', was not located in the output:\nsys.stdout:\n{}\nsys.stderr:\n{}"
+            self.error(msg, text_in, stdout, stderr)
+
+        text_not_in = self.getParam('text_not_in')
+        if (text_not_in is not None) and ((text_not_in in stdout) or (text_not_in in stderr)):
+            msg = "The content of 'text_not_in' parameter, '{}', was located in the output:\nsys.stdout:\n{}\nsys.stderr:\n{}"
+            self.error(msg, text_not_in, stdout, stderr)
+
+        # STDOUT
         text_in = self.getParam('text_in_stdout')
         if (text_in is not None) and (text_in not in stdout):
             msg = "The content of 'text_in_stdout' parameter, '{}', was not located in the output:\n{}"
@@ -29,8 +45,9 @@ class ConsoleDiff(Differ):
         text_not_in = self.getParam('text_not_in_stdout')
         if (text_not_in is not None) and (text_not_in in stdout):
             msg = "The content of 'text_not_in_stdout' parameter, '{}', was located in the output:\n{}"
-            self.error(msg, text_in, stdout)
+            self.error(msg, text_not_in, stdout)
 
+        # STDERR
         text_in = self.getParam('text_in_stderr')
         if (text_in is not None) and (text_in not in stderr):
             msg = "The content of 'text_in_stderr' parameter, '{}', was not located in the output:\n{}"
@@ -39,4 +56,32 @@ class ConsoleDiff(Differ):
         text_not_in = self.getParam('text_not_in_stderr')
         if (text_not_in is not None) and (text_not_in in stderr):
             msg = "The content of 'text_not_in_stderr' parameter, '{}', was located in the output:\n{}"
-            self.error(msg, text_in, stderr)
+            self.error(msg, text_not_in, stderr)
+
+        # RE
+        flags = 0
+        for flag in self.getParam('re_flags'):
+            flags |= eval(f're.{flag}')
+
+        re_match = self.getParam('re_match')
+        if re_match is not None:
+            match = re.match(re_match, stdout, flags=flags) or re.match(re_match, stderr, flags=flags)
+            if not match:
+                msg = "The regular expression of 're_match' parameter, '{}', did not produce a match in the output:\nsys.stdout:\n{}\nsys.stderr:\n{}"
+                self.error(msg, re_match, stdout, stderr)
+
+        # RE STDOUT
+        re_match = self.getParam('re_match_stdout')
+        if re_match is not None:
+            match = re.match(re_match, stdout, flags=flags)
+            if not match:
+                msg = "The regular expression of 're_match' parameter, '{}', did not produce a match in the output:\n{}"
+                self.error(msg, re_match, stdout)
+
+        # RE STDERR
+        re_match = self.getParam('re_match_stderr')
+        if re_match is not None:
+            match = re.match(re_match, stderr, flags=flags)
+            if not match:
+                msg = "The regular expression of 're_match' parameter, '{}', did not produce a match in the output:\n{}"
+                self.error(msg, re_match, stderr)
---
+++ moosetools/moosetest/tests/differs/test_ConsoleDiff.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+import io
+import logging
+import unittest
+from unittest import mock
+from moosetools.moosetest.differs import ConsoleDiff
+
+
+class TestConsoleDiff(unittest.TestCase):
+    def testDefault(self):
+        obj = ConsoleDiff(name='diff')
+        obj.execute(0, '', '')
+        self.assertEqual(obj.status(), 0)
+
+    def testTextIn(self):
+        obj = ConsoleDiff(name='diff', text_in='andrew')
+        obj.execute(0, 'andrew', '')
+        self.assertEqual(obj.status(), 0)
+
+        obj.execute(0, '', 'andrew')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '', '')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_in' parameter, 'andrew', was not located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testTextNotIn(self):
+        obj = ConsoleDiff(name='diff', text_not_in='andrew')
+        obj.execute(0, 'bob', 'julie')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, 'andrew', '')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_not_in' parameter, 'andrew', was located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+        obj.reset()
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '', 'andrew')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_not_in' parameter, 'andrew', was located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testTextInStdout(self):
+        obj = ConsoleDiff(name='diff', text_in_stdout='andrew')
+        obj.execute(0, 'andrew', 'andrew')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '', 'andrew')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_in_stdout' parameter, 'andrew', was not located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testTextNotInStdout(self):
+        obj = ConsoleDiff(name='diff', text_not_in_stdout='andrew')
+        obj.execute(0, 'bob', 'andrew')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, 'andrew', 'andrew')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_not_in_stdout' parameter, 'andrew', was located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testTextInStderr(self):
+        obj = ConsoleDiff(name='diff', text_in_stderr='andrew')
+        obj.execute(0, 'andrew', 'andrew')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, 'andrew', '')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_in_stderr' parameter, 'andrew', was not located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testTextNotInStderr(self):
+        obj = ConsoleDiff(name='diff', text_not_in_stderr='andrew')
+        obj.execute(0, 'andrew', 'bob')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, 'andrew', 'andrew')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The content of 'text_not_in_stderr' parameter, 'andrew', was located in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testReMatch(self):
+        obj = ConsoleDiff(name='diff', re_match='\d{4}-\d{2}-\d{2}')
+        obj.execute(0, '1980-06-24', '198-06-24')
+        self.assertEqual(obj.status(), 0)
+
+        obj.execute(0, '198-06-24', '1980-06-24')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '198-06-24', '')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The regular expression of 're_match' parameter, '\d{4}-\d{2}-\d{2}', did not produce a match in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+        obj.reset()
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '', '198-06-24')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The regular expression of 're_match' parameter, '\d{4}-\d{2}-\d{2}', did not produce a match in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testReMatchStdout(self):
+        obj = ConsoleDiff(name='diff', re_match_stdout='\d{4}-\d{2}-\d{2}')
+        obj.execute(0, '1980-06-24', '1980-06-24')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '198-06-24', '1980-06-24')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The regular expression of 're_match' parameter, '\d{4}-\d{2}-\d{2}', did not produce a match in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+    def testReMatchStderr(self):
+        obj = ConsoleDiff(name='diff', re_match_stderr='\d{4}-\d{2}-\d{2}')
+        obj.execute(0, '1980-06-24', '1980-06-24')
+        self.assertEqual(obj.status(), 0)
+
+        with self.assertLogs(level='ERROR') as log:
+            obj.execute(0, '1980-06-24', '198-06-24')
+        self.assertEqual(len(log.output), 1)
+        self.assertIn("The regular expression of 're_match' parameter, '\d{4}-\d{2}-\d{2}', did not produce a match in the output", log.output[0])
+        self.assertEqual(obj.status(), 1)
+
+if __name__ == '__main__':
+    unittest.main(module=__name__, verbosity=2)
